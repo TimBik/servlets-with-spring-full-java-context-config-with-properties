6e21dde94b756200dca62a49ee515e09f09ce0c7
TimBik <timurbikmullin@mail.ru>
2020-06-18 16:04:43 +0300

    add more comments

--- src/main/java/ru/itis/servlets/config/ApplicationContextConfig.java
+++ src/main/java/ru/itis/servlets/config/ApplicationContextConfig.java
@@ -15,22 +15,47 @@ import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 
+// говорим, что этот класс
+// используется для создания бинов
 @Configuration
+
+// указываем, где находится файлы properties
 @PropertySource("classpath:application.properties")
+
+// говорим, где искать классы,
+// которые так же будут создавать бины,
+// например классы с аннотациями
+// component, repository
 @ComponentScan(basePackages = "ru.itis.servlets")
+
 public class ApplicationContextConfig {
 
+    //используем, для получения
+    // доступа к properties
     @Autowired
     private Environment environment;
 
+
+    // кладем этот объект в контейнер
+    // тоесть при анотации
+    // @Autowired JdbcTemplate ...
+    // будет подтягиваться
+    // этот singleton объект
     @Bean
     public JdbcTemplate jdbcTemplate() {
         return new JdbcTemplate(hikariDataSource());
     }
 
+    // говорим как создавать DataSource
+    // используем встроенную в спринг
+    // реаизацию
     @Bean
     public DataSource driverManagerDataSource() {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
+
+        // по данным из
+        // файла application.properties
+        // настраиваем объект dataSource
         dataSource.setDriverClassName(environment.getProperty("db.driver"));
         dataSource.setUrl(environment.getProperty("db.url"));
         dataSource.setUsername(environment.getProperty("db.user"));
@@ -38,9 +63,15 @@ public class ApplicationContextConfig {
         return dataSource;
     }
 
+    // говорим как инициализируется объект HikariConfig,
+    // после кладем в контейнер бинов
     @Bean
     public HikariConfig hikariConfig() {
         HikariConfig config = new HikariConfig();
+
+        // по данным из
+        // файла application.properties
+        // настраиваем объект config
         config.setJdbcUrl(environment.getProperty("db.url"));
         config.setUsername(environment.getProperty("db.user"));
         config.setPassword(environment.getProperty("db.password"));
@@ -48,6 +79,9 @@ public class ApplicationContextConfig {
         return config;
     }
 
+    // реализация DataSource через HikariCP
+    // какая и реализаций будет использована
+    // зависит от аннотации @Qualifier
     @Bean
     public DataSource hikariDataSource() {
         return new HikariDataSource(hikariConfig());
